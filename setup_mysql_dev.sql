575d9f87ed88a4b09988f7e967a53d394a5f9911
GideonBature <infoaboutgideon@gmail.com>
2023-11-21 13:16:48 +0100

    a script that prepares a MySQL server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
 -- Script that prepares a MySQL server
-
 -- Create a database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create a new user
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY hbnb_dev_pwd;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant priviledges
-
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
