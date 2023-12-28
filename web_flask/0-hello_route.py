382e73b5d4267e6e452438fd5a80a968bd0b7bae
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-28 22:38:38 +0000

    return hello

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Starts a Flask web application."""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Show a response when route is accessed."""
+
+    return 'Hello HBNB'
