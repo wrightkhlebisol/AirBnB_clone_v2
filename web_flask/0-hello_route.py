dfffd06eaf4290446fd79a0c53ea3cf6604f092e
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-25 14:45:57 +0000

    change home route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello():
     """Show a response when route is accessed."""
 
@@ -13,4 +13,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
